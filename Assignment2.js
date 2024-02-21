76ae92f6f862dd601dcba76a784ca739bc723eac
Rohan Dhalpe <rohandhalpe05@gmail.com>
2024-02-21 10:42:59 +0530

    Added assignment 6

---
+++ Assignment2.js
@@ -0,0 +1,32 @@
+//2. What makes a method mutating or non mutating in Javascript? 
+//   Find out whether each of the following methods are mutating or non-mutating. How can you verify this?:
+
+// 1) Push - Mutating
+  
+let arr1 = [2,4,6,8,10,12];
+arr1.push(14);
+console.log(arr1)
+
+//  2) Pop - Mutating
+
+let arr2 = [2,4,6,8,10,12];
+arr2.pop();
+console.log(arr2)
+
+//  3) Filter - Non-mutating
+
+const numbers = [1, 2, 3, 4, 5];
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers); // Output: [2, 4]
+
+//  4) Find - Non-mutating
+
+const nums = [1, 2, 3, 4, 5];
+const foundNumber = nums.find(num => num > 3);
+console.log(foundNumber); // Output: 4
+
+//  5) Sort - Mutating
+
+const sampArray = [1,10,7,6,5]
+sampArray.sort();
+console.log(sampArray)
